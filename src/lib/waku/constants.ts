c8aa4878ca695bf5ae5ee3d9f699fa3bd045b621
Danish Arora <danisharora099@gmail.com>
2025-09-10 15:04:38 +0530

    chore: update content topic

--- src/lib/waku/constants.ts
+++ src/lib/waku/constants.ts
@@ -2,7 +2,7 @@
  * Single content topic for all message types
  * Different message types are parsed from the message content itself
  */
-export const CONTENT_TOPIC = '/opchan-demo-1/1/messages/proto';
+export const CONTENT_TOPIC = '/opchan-demo/1/messages/proto';
 
 /**
  * Bootstrap nodes for the Waku network
