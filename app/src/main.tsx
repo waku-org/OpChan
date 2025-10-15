110cadd355fd40eb5d62a674d69f4476ec85161e
Danish Arora <danisharora099@gmail.com>
2025-10-15 16:41:58 +0530

    chore: simplify AppKit providers

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,20 +3,13 @@ import App from './App.tsx';
 import './index.css';
 import { Buffer } from 'buffer';
 import { OpChanProvider } from '@opchan/react';
-import { WagmiProvider } from 'wagmi';
-import { AppKitProvider } from '@reown/appkit/react';
-import { appkitConfig, config } from '@opchan/core';
 
 if (!(window as Window & typeof globalThis).Buffer) {
   (window as Window & typeof globalThis).Buffer = Buffer;
 }
 
 createRoot(document.getElementById('root')!).render(
-  <WagmiProvider config={config}>
-    <AppKitProvider {...appkitConfig}>
       <OpChanProvider config={{ ordiscanApiKey: '6bb07766-d98c-4ddd-93fb-6a0e94d629dd' }}>
         <App />
       </OpChanProvider>
-    </AppKitProvider>
-  </WagmiProvider>
 );
